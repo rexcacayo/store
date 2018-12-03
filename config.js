c47c38f45e6e87cc022629447b70c15fdbb06427
Pancho_rex <rflugaresir@gmail.com>
2018-12-03 11:48:17 +0000

    Segundo comit, exportando de plugin a repositorio

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'android',
+		wp_ws_url : 'http://store.entornodemo.com/wp-appkit-api/android',
+		wp_url : 'http://store.entornodemo.com',
+		theme : 'q-android',
+		version : '1.0.0',
+		app_type : 'phonegap-build',
+		app_title : 'STORE',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'B$a(<?CUX&e|a-(iLf;yjk{!ml<w=a*gBQ|R|=~yDKlO^HQkK8`*:eXn~&*7UwwR',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
